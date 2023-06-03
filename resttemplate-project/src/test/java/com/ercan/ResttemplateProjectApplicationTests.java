ace50055249dd0b786ddd74267675961dd4af1fc
Ercan Karakaya <karakayaercan71@gmail.com>
2023-06-03 16:20:14 +0300

    initial commit

---
+++ resttemplate-project/src/test/java/com/ercan/ResttemplateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ercan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttemplateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
